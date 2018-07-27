b3f9e0752ff2f5b6b500f3a973314fc6fcf270ad
SizakeYang <31911344+SizakeYang@users.noreply.github.com>
2018-07-27 17:45:28 +0800

    http --> https;

---
+++ src/main/java/com/sizake/ebank/config/TomcatHttpConfig.java
@@ -0,0 +1,71 @@
+package com.sizake.ebank.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatHttpConfig {
+
+    @Value("${from.server.http.port}")
+    private Integer httpPort;
+
+    @Value("${server.port}")
+    private Integer httpsPort;
+
+    /**
+     * 配置内置的servlet容器工厂为tomcat.
+     *
+     * @return
+     */
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        final TomcatEmbeddedServletContainerFactory
+                tomcat = new TomcatEmbeddedServletContainerFactory() {
+
+            @Override
+            protected void
+            postProcessContext(final Context context) {
+
+                final SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                final SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        //添加连接配置，主要是http的配置信息.
+        tomcat.addAdditionalTomcatConnectors(this.initiateHttpConnector());
+
+        //Tomcat large file upload connection reset
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+
+        return tomcat;
+    }
+
+    /**
+     * 配置一个http连接信息.
+     */
+    private Connector initiateHttpConnector() { // for RedirectPort
+        final Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(this.httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(this.httpsPort);// http 302
+        return connector;
+    }
+}
