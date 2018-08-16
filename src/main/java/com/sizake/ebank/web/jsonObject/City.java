34f26ff736980468499722c3ec95d7058b7d25ea
SizakeYang <31911344+SizakeYang@users.noreply.github.com>
2018-08-16 15:07:15 +0800

    mybatis alias

--- src/main/java/com/sizake/ebank/web/jsonObject/City.java
+++ src/main/java/com/sizake/ebank/web/jsonObject/City.java
@@ -3,10 +3,12 @@ package com.sizake.ebank.web.jsonObject;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
 
 @Data
 @NoArgsConstructor//-->当有@AllArgsConstructor时,要有@NoArgsConstructor,否则会出现Jackson对RequestBody反序列化失败
 @AllArgsConstructor
+@Alias("mysql_city")
 public class City {
     private String id;
     private String name;
